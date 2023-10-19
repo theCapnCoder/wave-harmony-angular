5cbcb11e61099a8c916f82e6ab9e882675e57de4
PavelM <myagkiypavel@gmail.com>
2023-10-19 20:08:38 +0300

    feat: variables in components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { HeroesComponent } from './test/heroes/heroes.component';
 
 @NgModule({
   declarations: [
@@ -32,6 +33,7 @@ import { MatExpansionModule } from '@angular/material/expansion';
     OrderComponent,
     ProfileComponent,
     FooterComponent,
+    HeroesComponent,
   ],
   imports: [
     BrowserModule,
